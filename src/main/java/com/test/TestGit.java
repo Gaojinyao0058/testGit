18e2c577df36b2e42d01845ede1c7d8eaff4f337
Mr.Gao <563590803@qq.com>
2022-05-05 01:43:34 +0800

    IDEA sencond commit.

--- src/main/java/com/test/TestGit.java
+++ src/main/java/com/test/TestGit.java
@@ -3,5 +3,6 @@ package com.test;
 public class TestGit {
     public static void main(String[] args) {
         System.out.println("test Git 111 !");
+        System.out.println("test Git 222 !");
     }
 }
